496532a18e018ce13a9e63d133eb3c56fa7ea99b
梁斌 <171830506@smail.nju.edu.cn>
2020-06-08 16:04:29 +0800

    完成所有接口

--- iot_manager/src/main/java/com/example/iot_manager/controller/ModelController.java
+++ iot_manager/src/main/java/com/example/iot_manager/controller/ModelController.java
@@ -1,7 +1,54 @@
 package com.example.iot_manager.controller;
 
+import com.example.iot_manager.data.ModelDo;
+import com.example.iot_manager.service.ModelService;
+import com.example.iot_manager.vo.ModelVO;
+import com.example.iot_manager.vo.ResponseVO;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/model")
 public class ModelController {
+
+  final
+  ModelService modelService;
+
+  @Autowired
+  public ModelController(ModelService modelService) {
+    this.modelService = modelService;
+  }
+
+  @PostMapping("/add")
+  public ResponseVO<String> addModel(@RequestBody ModelVO modelVO){
+    return modelService.addModel(modelVO);
+  }
+
+  @PostMapping("/update")
+  public ResponseVO<String> updateModel(@RequestParam int modelId, @RequestBody ModelVO modelVO){
+    return modelService.updateModel(modelId, modelVO);
+  }
+
+  @DeleteMapping("/delete")
+  public ResponseVO<String> deleteModel(int modelId){
+    return modelService.deleteModel(modelId);
+  }
+
+  @GetMapping("/getall")
+  public ResponseVO<List<ModelDo>> getAllModel(int page, int size){
+    return modelService.findAllModel(page, size);
+  }
+
+  @GetMapping("/get")
+  public ResponseVO<ModelDo> getModel(int modelId){
+    return modelService.findModel(modelId);
+  }
+
 }
--- iot_manager/src/main/java/com/example/iot_manager/data/ModelDo.java
+++ iot_manager/src/main/java/com/example/iot_manager/data/ModelDo.java
@@ -1,5 +1,6 @@
 package com.example.iot_manager.data;
 
+import com.example.iot_manager.vo.ModelVO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,39 +16,54 @@ import java.util.Date;
 @Table(name = "manager_model")
 public class ModelDo implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int modelId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int modelId;
 
-    private String modelName;
+  private String modelName;
 
-    private String attrName;
+  private String attrName;
 
-    private String attrIdentifier;
+  private String attrIdentifier;
 
-    private String dataType;
+  private String dataType;
 
-    private int dataLen;
+  private int dataLen;
 
-    private String dataUnit;
+  private String dataUnit;
 
-    private int dataMin;
+  private int dataMin;
 
-    private int dataMax;
+  private int dataMax;
 
-    private String modelDescription;
+  private String modelDescription;
 
-    private String serviceName;
+  private String serviceName;
 
-    private String serviceIdentifier;
+  private String serviceIdentifier;
 
-    private int callMethod;
+  private int callMethod;
 
-    private String serviceDescription;
+  private String serviceDescription;
 
-    @Column(name = "create_time",insertable = false,updatable = false,columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-    private Date createTime;
+  @Column(name = "create_time", insertable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+  private Date createTime;
 
-    @Column(name = "update_time",insertable = false,updatable = false,columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
-    private Date updateTime;
+  @Column(name = "update_time", insertable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+  private Date updateTime;
+
+  public void setModelVO(ModelVO modelVO) {
+    this.modelName = modelVO.getModelName();
+    this.attrName = modelVO.getAttrName();
+    this.attrIdentifier = modelVO.getAttrIdentifier();
+    this.dataType = modelVO.getDataType();
+    this.dataLen = modelVO.getDataLen();
+    this.dataMin = modelVO.getDataMin();
+    this.dataMax = modelVO.getDataMax();
+    this.modelDescription = modelVO.getModelDescription();
+    this.serviceName = modelVO.getServiceName();
+    this.serviceIdentifier = modelVO.getServiceIdentifier();
+    this.callMethod = modelVO.getCallMethod();
+    this.serviceDescription = modelVO.getServiceDescription();
+  }
 }
