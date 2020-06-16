49fb33fa1f64335a25cc12c928fe23b6deead0c3
171250685@smail.nju.edu.cn <wawj197152>
2020-06-16 11:02:44 +0800

    添加场景联动

---
+++ iot_rule/src/main/java/com/example/iot_rule/sceneService/po/ScenePO.java
@@ -0,0 +1,21 @@
+package com.example.iot_rule.sceneService.po;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Name:
+ * DESCRIPTION:
+ *
+ * @author 孙刘博
+ * @date 2020/6/13 14:16
+ * @Version 1.0
+ */
+@Setter
+@Getter
+public class ScenePO {
+    private int id;
+    private int deviceId;//绑定的设备id,比如温度报警器
+    private String condition;
+    private String action;
+}
